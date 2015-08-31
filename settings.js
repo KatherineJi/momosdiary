85dd54e53f60aa4752cddbd1e4cd8f6d4882e5e6
Katherine Ji <kabuluode@163.com>
2015-08-31 17:07:29 +0800

    Test7

--- settings.js
+++ settings.js
@@ -1,13 +1,11 @@
 /**
  * Created by Momo on 15/8/5.
  */
-
-
 module.exports = {
     cookieSecret: 'myBlog',
     db_name : 'blog', //数据库名称
     db_host : 'localhost', //数据库地址
     db_port : 27017,  // 数据库端口
     username: 'momo',
-    password: 'momo0828'
+    password: 'PWD'
 };
