47eaa184a07897f4d0c004eab9a781155afab6a2
Katherine Ji <kabuluode@163.com>
2015-09-23 16:33:51 +0800

    add links file

--- settings.js
+++ settings.js
@@ -1,20 +1,20 @@
 /**
  * Created by Momo on 15/8/5.
  */
-//module.exports = {
-//    cookieSecret: 'myBlog',
-//    db_name : 'YmtkddjHYDqLAwcKaNoh', //数据库名称
-//    db_host : 'mongo.duapp.com', //数据库地址
-//    db_port : '8908', // 数据库端口
-//    username : 'ebf90fdcb5944aa0bb9b325fc98b1486', //用户AK
-//    password : 'a6a91fea23284e6a9e7b9a6135207055' //用户SK
-//};
-
 module.exports = {
     cookieSecret: 'myBlog',
-    db_name : 'blog', //数据库名称
-    db_host : 'localhost', //数据库地址
-    db_port : 27017,  // 数据库端口
-    username: 'momo',
-    password: 'momo0828'
+    db_name : 'YmtkddjHYDqLAwcKaNoh', //数据库名称
+    db_host : 'mongo.duapp.com', //数据库地址
+    db_port : '8908', // 数据库端口
+    username : 'ebf90fdcb5944aa0bb9b325fc98b1486', //用户AK
+    password : 'a6a91fea23284e6a9e7b9a6135207055' //用户SK
 };
+
+//module.exports = {
+//    cookieSecret: 'myBlog',
+//    db_name : 'blog', //数据库名称
+//    db_host : 'localhost', //数据库地址
+//    db_port : 27017,  // 数据库端口
+//    username: 'momo',
+//    password: 'momo0828'
+//};
